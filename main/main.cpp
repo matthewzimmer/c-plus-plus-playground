9335b043efe3975486df0c14978b56c396fc2730
Matthew Zimmer <matthewz@channeleyes.com>
2017-03-03 22:22:02 -0500

    Ignoring jetbrains IDE metadata. implemented super simple main.cpp program printing out hello, world to cout.

---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Matthew Zimmer on 3/3/17.
+//
+
+#include "main.h"
+
+int main() {
+    cout << "Hello, world!" << endl;
+}
---
+++ main/main.h
@@ -0,0 +1,11 @@
+//
+// Created by Matthew Zimmer on 3/3/17.
+//
+
+#ifndef C_PLUS_PLUS_PLAYGROUND_MAIN_H
+#define C_PLUS_PLUS_PLAYGROUND_MAIN_H
+
+#include <iostream>
+using namespace std;
+
+#endif //C_PLUS_PLUS_PLAYGROUND_MAIN_H
